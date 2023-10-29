96bee0c9073c5a553b25231955ed174d713f2324
Deandra <57567468+xvxvdee@users.noreply.github.com>
2023-10-29 14:17:45 -0400

    Added back object detection and added comments to code

--- lab1_sketch.ino
+++ lab1_sketch.ino
@@ -1,7 +1,7 @@
 #include <NewPing.h>
 
 #define MAX_DISTANCE 5000
-#define left digitalRead(A4)
+#define left digitalRead(A4) // Line Sensors
 #define middle digitalRead(A5)
 #define right digitalRead(2)
 // #define IRR digitalRead(8)
@@ -19,45 +19,33 @@ int PWMBL=6; // left motors B pin
 int high = 72;
 int turnHigh = 90;
 int low = 0;
-// int trigPinL = 13; // Connect the Trig pin of the left Ultrasonic Sensor to pin 9 on Arduino
-// int echoPinL = 12; // Connect the Echo pin of the left Ultrasonic Sensor to pin 10 on Arduino
-// int trigPinR = 8; // Connect the Trig pin of the right Ultrasonic Sensor to pin 8 on Arduino
-// int echoPinR = 7; // Connect the Echo pin of the right Ultrasonic Sensor to pin 7 on Arduino
+
+// Trig and Echo pins for Ultrasonic Sensors
 int trigPinF = 3; // Connect the Trig pin of the front-facing Ultrasonic Sensor to pin 3 on Arduino
 int echoPinF = 4; // Connect the Echo pin of the front-facing Ultrasonic Sensor to pin 4 on Arduino
 int echoPinFR = A0; // Connect the Trig pin of the right front-facing Ultrasonic Sensor to pin A0 on Arduino
 int trigPinFR = A1; // Connect the Echo pin of the right front-facing Ultrasonic Sensor to pin A1 on Arduino
 int echoPinFL = A2; // Connect the Trig pin of the left front-facing Ultrasonic Sensor to pin A2 on Arduino
 int trigPinFL = A3; // Connect the Echo pin of the left front-facing Ultrasonic Sensor to pin A3 on Arduino
-// int echoPinB = A5; // Connect the Echo pin of the left front-facing Ultrasonic Sensor to pin A5 on Arduino
-// int trigPinB = A4; // Connect the Trig pin of the left front-facing Ultrasonic Sensor to pin A4 on Arduino
-
 
 // int left = A4;
 // int right = A5;
 // int middle = 2;
-  long durationR, durationL, durationF, distanceF, durationFR, distanceFR, distanceFL, durationFL, durationB, distanceB;
-  unsigned int distanceR, distanceL;
-// NewPing sonarL(trigPinL, echoPinL, MAX_DISTANCE);
-// NewPing sonarR(trigPinR, echoPinR, MAX_DISTANCE);
+long durationR, durationL, durationF, distanceF, durationFR, distanceFR, distanceFL, durationFL, durationB, distanceB;
+unsigned int distanceR, distanceL;
+
+// Ultrasonic Sensors
 NewPing sonarFL(trigPinFL, echoPinFL, MAX_DISTANCE);
 NewPing sonarFR(trigPinFR, echoPinFR, MAX_DISTANCE);
 NewPing sonarF(trigPinF, echoPinF, MAX_DISTANCE);
 // NewPing sonarB(trigPinB, echoPinB, MAX_DISTANCE);
 
-// NewPing sonar(trigPinF, echoPinF, MAX_DISTANCE);
-// NewPing sonar[2] = {     // Sensor object array.
-//   NewPing(trigPinL, echoPinL, MAX_DISTANCE), // Each sensor's trigger pin, echo pin, and max distance to ping.
-//   NewPing(trigPinR, echoPinR, MAX_DISTANCE),
-// }
-
-
 void setup () {
   Serial.begin(9600);   // initialize the serial communications:
 
   // pinMode (AIN1, OUTPUT);
-  pinMode (BIN1, OUTPUT);
-  pinMode (STBY, OUTPUT);
+  pinMode(BIN1, OUTPUT);
+  pinMode(STBY, OUTPUT);
   pinMode(PWMAR, OUTPUT); 
   pinMode(PWMBR, OUTPUT); 
   pinMode (PWMAL, OUTPUT);
@@ -70,10 +58,6 @@ void setup () {
   analogWrite(PWMBL, high);
 
   // Initialize the Ultrasonic Sensor pins
-  // pinMode(trigPinL, OUTPUT);
-  // pinMode(echoPinL, INPUT);
-  // pinMode(trigPinR, OUTPUT);
-  // pinMode(echoPinR, INPUT);
   pinMode(trigPinF, OUTPUT);
   pinMode(echoPinF, INPUT);
   pinMode(trigPinFR, OUTPUT);
@@ -83,6 +67,7 @@ void setup () {
   // pinMode(trigPinB, OUTPUT);
   // pinMode(echoPinB, INPUT);
 
+// IR Sensors
   pinMode(IRR, INPUT);
   pinMode(IRL, INPUT);
 
@@ -92,45 +77,33 @@ void setup () {
 void loop () {
   delay(50);
   // Add this code where you want to read from the sensor
-  // distanceR = sonarR.ping_cm();
-  // distanceL = sonarL.ping_cm();
+
   distanceFR = sonarFR.ping_cm();
   distanceFL = sonarFL.ping_cm();
   distanceF = sonarF.ping_cm();
+  // distanceB = sonarB.ping_cm();
 
   int IRRStatus = digitalRead(IRR);
   int IRLStatus = digitalRead(IRL);
-  // distanceB = sonarB.ping_cm();
 
-  // distanceR = findDistance(trigPinR, echoPinR);
-  // distanceL = findDistance(trigPinL, echoPinL);
-  // distanceF = findDistance(trigPinF, echoPinF);
-  // distanceFR = findDistance(trigPinFR, echoPinFR);
-  // distanceFL = findDistance(trigPinFL, echoPinFL);
-  // distanceB = findDistance(trigPinB, echoPinB);
-  
-  // Serial.print("Distance Right--: ");
-  // Serial.println(distanceR);  // Tools>Serial Monitor
-  // Serial.print("Distance Left----: ");
-  // Serial.println(distanceL);  // Tools>Serial Monitor
-  // Serial.print("Distance Front----: ");
-  // Serial.println(distanceF);  // Tools>Serial Monitor
-  // Serial.print("Distance Front Right----: ");
-  // Serial.println(distanceFR);  // Tools>Serial Monitor
-  // Serial.print("Distance Front Left----: ");
-  // Serial.println(distanceFL);  // Tools>Serial Monitor
-  // Serial.print("Distance Back----: ");
-  // Serial.println(distanceB);  // Tools>Serial Monitor
+// Tools>Serial Monitor
+  Serial.print("Distance Front ----: ");
+  Serial.println(distanceF);  
+  Serial.print("Distance Front Right----: ");
+  Serial.println(distanceFR);  
+  Serial.print("Distance Front Left----: ");
+  Serial.println(distanceFL);  
+  // Serial.println(distanceB);  
   // Serial.print("Left On--------:");
   // Serial.println(left);
   // Serial.print("Middle On--------:");
   // Serial.println(middle);
   // Serial.print("Right On--------:");
   // Serial.println(right);
-  Serial.print("IR Right On--------:");
-  Serial.println(IRRStatus);
-  Serial.print("IR Left On--------:");
-  Serial.println(IRLStatus);
+  // Serial.print("IR Right On--------:");
+  // Serial.println(IRRStatus);
+  // Serial.print("IR Left On--------:");
+  // Serial.println(IRLStatus);
 
 
   if (IRRStatus == HIGH && IRLStatus == HIGH) {
@@ -151,21 +124,21 @@ void loop () {
   // // } else if (distanceB > 80 && distanceL < 40 && distanceR < 40) {
   // //   Forward();
   // //   delay(300);
-  // } else if (distanceF <= 20 && distanceF != 0) { // 30 might be too much
-  //   // Backward();
-  //   // delay(300);
-  //   Right();
-  //   delay(750);
-  // } else if (distanceFR <= 18 && distanceFR != 0) {
-  //   // Backward();
-  //   // delay(300);
-  //   Left();
-  //   delay(300);
-  // } else if (distanceFL <= 18 && distanceFL != 0) {
-  //   // Backward();
-  //   // delay(300);
-  //   Right();
-  //   delay(300);
+  } else if (distanceF <= 20 && distanceF != 0) { // 30 might be too much
+    // Backward();
+    // delay(300);
+    Right();
+    delay(750);
+  } else if (distanceFR <= 18 && distanceFR != 0) {
+    // Backward();
+    // delay(300);
+    Left();
+    delay(300);
+  } else if (distanceFL <= 18 && distanceFL != 0) {
+    // Backward();
+    // delay(300);
+    Right();
+    delay(300);
   // } else if((distanceB < 15 || distanceB > 80) && distanceFR <= 15 ){
   //   Forward();
   //   delay(300);
@@ -269,17 +242,3 @@ void SlightLeft () {
   digitalWrite (STBY, HIGH);
 
 }
-
-long findDistance(int trigPin, int echoPin) {
-  digitalWrite(trigPin, LOW);  
-  delayMicroseconds(2); 
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10); 
-  digitalWrite(trigPin, LOW);
-  long duration = pulseIn(echoPin, HIGH);
-  return ((duration/2) / 29.1);
-}
-
-// long findDistance2() {
-//   sonar1.ping_cm()
-// }
